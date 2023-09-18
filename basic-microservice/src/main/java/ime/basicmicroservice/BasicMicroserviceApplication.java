677fc4064605141b2beda3396a3cad23a0f46e9d
IvanM <ivan.montes@outlook.com>
2023-09-18 18:54:27 +0200

    Initial basic-microservice commit

---
+++ basic-microservice/src/main/java/ime/basicmicroservice/BasicMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package ime.basicmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicMicroserviceApplication.class, args);
+	}
+
+}
