c2559cedc7da6b826059bdc82f8758c0f5fa4892
IvanM <ivan.montes@outlook.com>
2023-09-18 18:35:18 +0200

    Initial commit eureka

---
+++ eureka-server/src/main/java/ime/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package ime.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
