b205d77ec3d5990757d13f164511da149ebf1fa2
IvanM <ivan.montes@outlook.com>
2023-09-22 17:35:31 +0200

    Add tag @RefreshScope

--- basic-microservice/src/main/java/ime/basicmicroservice/controller/MainController.java
+++ basic-microservice/src/main/java/ime/basicmicroservice/controller/MainController.java
@@ -1,5 +1,6 @@
 package ime.basicmicroservice.controller;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/main")
+@RefreshScope
 public class MainController {
 
 	
