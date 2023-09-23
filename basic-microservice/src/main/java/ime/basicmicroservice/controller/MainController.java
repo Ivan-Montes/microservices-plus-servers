feb761c2f089ec55e21b6bf44e230b866e7dc09f
IvanM <ivan.montes@outlook.com>
2023-09-23 12:46:25 +0200

    Configuring open feign

--- basic-microservice/src/main/java/ime/basicmicroservice/controller/MainController.java
+++ basic-microservice/src/main/java/ime/basicmicroservice/controller/MainController.java
@@ -1,10 +1,14 @@
 package ime.basicmicroservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ime.basicmicroservice.client.MediumClient;
 
 
 @RestController
@@ -12,11 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class MainController {
 
+
+	@Value("${main.creator.name}")
+	private String creatorName; 
+	
+	@Autowired
+	private MediumClient mediumClient;
 	
 	@GetMapping
 	public ResponseEntity<String> fnHello() {
-		return ResponseEntity.ok("Hello, it's me");
+		return ResponseEntity.ok("Hello, it's me " + creatorName);
 	}
 	
-	
+	@GetMapping("/medium")
+	public ResponseEntity<String> fnBye(){
+		return ResponseEntity.ok(mediumClient.sayBye());
+	}
 }
